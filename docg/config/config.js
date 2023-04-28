043382d16126ef2b0ef4174c859227ff14229560
Henrique Alexandre de Camargo <henriquealexandredec@gmail.com>
2023-04-28 20:23:56 -0300

    teste

---
+++ docg/config/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  development: {
+    username: 'henrique',
+    password: '123',
+    database: 'docg',
+    host: 'localhost',
+    dialect: 'mysql',
+  },
+  test: {
+    username: 'henrique',
+    password: '123',
+    database: 'docg_test',
+    host: 'localhost',
+    dialect: 'mysql',
+  },
+  production: {
+    username: 'henrique',
+    password: '123',
+    database: 'docg_prod',
+    host: 'localhost',
+    dialect: 'mysql',
+  },
+};
